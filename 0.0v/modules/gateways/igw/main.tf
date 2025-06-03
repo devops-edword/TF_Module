6659db97233feae971f56d16a818738835bf3347
Edword-Shin <adwordshin@gmail.com>
2025-06-04 04:44:04 +0900

    TF Module Commit :: 0.0v main.tf(module gateways-igw) Modify2 Commit

--- 0.0v/modules/gateways/igw/main.tf
+++ 0.0v/modules/gateways/igw/main.tf
@@ -8,6 +8,6 @@ resource "aws_internet_gateway" "igw" {
 
 
 resource "aws_internet_gateway_attachment" "igw_attach" {
-  internet_gateway_id = aws_internet_gateway.igw
+  internet_gateway_id = aws_internet_gateway.igw.id
   vpc_id = var.vpc-id
 }
