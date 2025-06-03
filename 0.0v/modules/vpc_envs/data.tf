77f24d85f78e048c9aa9095c3a96b39cdf67407b
Edword-Shin <adwordshin@gmail.com>
2025-06-04 01:10:28 +0900

    TF Module Commit :: 0.0v - data.tf(module vpc_env) Modify Commit

--- 0.0v/modules/vpc_envs/data.tf
+++ 0.0v/modules/vpc_envs/data.tf
@@ -2,3 +2,38 @@
 data "aws_availability_zones" "az" {
   state = "available"
 }
+
+
+# Data Block for EC2 Instances
+data "aws_ami" "amzn2023" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+
+data "aws_ami" "ubuntu2204" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical ID
+}
